066ea654647e6358e9ed50b157489838df4c3032
Abhijit Jagadeesh <abjagade@synamedia.com>
2021-04-20 19:27:48 +0100

    add players names, validation, refactor

--- index.js
+++ index.js
@@ -1,59 +1,47 @@
-var playersInfo = {
-  player1: 0,
-  player2: 0,
-};
-
-currentPlayer = 1;
-
-function getTotalPlayers() {
-  var count = 0;
-  for (var i in playersInfo) {
-    if (playersInfo.hasOwnProperty(i)) count++;
-  }
-  return count;
-}
-
-totalPlayers = getTotalPlayers();
+var score = {};
+var players = [];
+var currentPlayer = 0;
+var totalPlayers = 0;
 
 function rollANumber() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
 function updateScore(rolledNumber) {
-  playersInfo["player" + currentPlayer] += rolledNumber;
+  score[players[currentPlayer]] += rolledNumber;
 }
 
 function updateScoreDisplay() {
   document.getElementById("player" + currentPlayer + "-score").textContent =
-    playersInfo["player" + currentPlayer];
+    score[players[currentPlayer]];
 }
 
 function updateTurn() {
-  if (currentPlayer < totalPlayers) {
-    currentPlayer += 1;
+  if (currentPlayer >= totalPlayers - 1) {
+    currentPlayer = 0;
   } else {
-    currentPlayer = 1;
+    currentPlayer += 1;
   }
   document.getElementById("player-turn").textContent =
-    "Player" + currentPlayer + " Turn";
+    players[currentPlayer] + "'s turn to roll !";
 }
 
 function resetPlayersScore(player) {
-  document.getElementById(player + "-score").textContent = 0;
+  document.getElementById("player" + player + "-score").textContent = 0;
 }
 
 function reset() {
-  currentPlayer = 0;
+  currentPlayer = 1;
   updateTurn();
-  for (var i in playersInfo) {
-    playersInfo[i] = 0;
+  for (var i in players) {
+    score[players[i]] = 0;
     resetPlayersScore(i);
   }
 }
 
 function checkResult() {
-  for (var i in playersInfo) {
-    if (playersInfo[i] >= 20) {
+  for (var i in score) {
+    if (score[i] >= 20) {
       return "Winner";
     }
   }
@@ -64,10 +52,49 @@ function displayWinner() {
   document.getElementById("gameover").style.display = "block";
   document.getElementById("game").style.display = "none";
   document.getElementById("winner").innerHTML =
-    "Hurray Player " +
-    currentPlayer +
+    "Hurray " +
+    players[currentPlayer] +
     " reached 20 First and wins this round !";
 }
+
+function setUpGame() {
+  players[0] = document.getElementById("player1").value;
+  players[1] = document.getElementById("player2").value;
+  score[players[0]] = 0;
+  score[players[1]] = 0;
+
+  totalPlayers = players.length;
+  document.getElementById("player-turn").textContent =
+    players[0] + "'s turn to roll !";
+  document.getElementById("first-player-score").textContent =
+    players[0] + "'s score:";
+  document.getElementById("second-player-score").textContent =
+    players[1] + "'s score:";
+}
+
+function validPlayerNames() {
+  for (var i in players) {
+    if (players[i] === "") {
+      return false;
+    }
+  }
+  return true;
+}
+
+document
+  .getElementById("add-players-button")
+  .addEventListener("click", function () {
+    setUpGame();
+    if (validPlayerNames()) {
+      // setUpGame();
+      document.getElementById("add-players").style.display = "none";
+      document.getElementById("game").style.display = "block";
+    } else {
+    }
+    document.getElementById("message").textContent =
+      "Players Names cannot be empty";
+    document.getElementById("message").style.color = "red";
+  });
 
 document.getElementById("roll-button").addEventListener("click", function () {
   let rolledNumber = rollANumber();
